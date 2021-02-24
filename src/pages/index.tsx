c59f148454fe82963f722452e42dc47b805ac9a3
leo-holanda <leo.uldholanda@gmail.com>
2021-02-24 18:50:45 -0300

    NLW Third Day

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,5 +1,6 @@
 import Head from 'next/head'
-import { CompletedChallenges } from '../components/CompleteChallenges';
+import { ChallengeBox } from '../components/ChallengeBox';
+import { CompletedChallenges } from '../components/CompletedChallenges';
 import { Countdown } from '../components/Countdown';
 import { ExperienceBar } from "../components/ExperienceBar";
 import { Profile } from "../components/Profile";
@@ -12,19 +13,19 @@ export default function Home() {
       <Head>
         <title>Inicio | move.it</title>
       </Head>
+
       <ExperienceBar />
     
-    <section>
-      <div>
-        <Profile />
-        <CompletedChallenges />
-        <Countdown/>
-      </div>
-      <div>
-
-      </div>
-    </section>
+      <section>
+        <div>
+          <Profile />
+          <CompletedChallenges />
+          <Countdown/>
+        </div>
+        <div>
+          <ChallengeBox/>
+        </div>
+      </section>
     </div>
-    
   )
 }
